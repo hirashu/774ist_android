5ff3e7a4b4fc92cfee4f0105eaa143c82027f4b2
hirashu <hirahara11@gmail.com>
2020-10-18 13:40:45 +0900

    再構築

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 774istのアンドロイドアプリ
 
 使用ライブラリ
-Glide：https://github.com/bumptech/glide
+Glide
+    https://github.com/bumptech/glide
+Dagger Hilt
+    https://github.com/google/dagger
--- app/src/main/java/com/example/a774ist/ui/home/MainScheduleFragment.kt
+++ app/src/main/java/com/example/a774ist/ui/home/MainScheduleFragment.kt
@@ -1,12 +1,10 @@
-package com.example.a774ist
+package com.example.a774ist.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
